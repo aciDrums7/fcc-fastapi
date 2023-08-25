6ea4ef53ac69f5d3489ce923e23dc1d3287fc38d
aciDrums7 <edoardo.carradori.96@gmail.com>
2023-08-25 15:50:03 +0200

    What is CORS?

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 """ fastAPI entrypoint """
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database.db_config import Base, Engine
 from .APIs.auth_api import router as auth_router
 from .APIs.users_api import router as users_router
@@ -14,8 +15,27 @@ load_dotenv(verbose=True)
 
 app = FastAPI()
 
+origins = [
+    "https://www.google.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(auth_router)
 app.include_router(users_router)
 app.include_router(posts_router)
 app.include_router(votes_router)
+
+
+@app.get("/", response_model=str)
+def hello_world():
+    return "Hello World!"
