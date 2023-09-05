53f245d2ed1df335581e56b5b884f7a74a282584
aciDrums7 <edoardo.carradori.96@gmail.com>
2023-09-05 22:42:36 +0200

    Test User Fixture

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,7 +1,19 @@
+import pytest
 from app.schemas.token_schemas import Token
 from tests.conftest import client
 from app.schemas.users_schemas import UserOut
 from app.utils.password_utils import verify_password
+
+
+@pytest.fixture
+def test_user(client):
+    new_user_data = {"email": "testuser@email.com", "password": "testPassword1"}
+    res = client.post("/users", json=new_user_data)
+
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user["plain_password"] = new_user_data["password"]
+    return new_user
 
 
 def test_hello_world(client):
@@ -22,12 +34,10 @@ def test_create_user(client):
     assert res.status_code == 201
 
 
-def test_login_user(client):
-    test_email = "test@email.com"
-    test_password = "testpassword"
-    res = client.post("/users", json={"email": test_email, "password": test_password})
+def test_login_user(client, test_user):
     res = client.post(
-        "/login", data={"username": test_email, "password": test_password}
+        "/login",
+        data={"username": test_user["email"], "password": test_user["plain_password"]},
     )
     print(res.json())
     new_token = Token(**res.json())
